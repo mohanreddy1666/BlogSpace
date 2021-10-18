9ce729f1b6d6571cd444bf155f19b2cfa029e6a4
mohanreddy1666 <mohannallamilli11@gmail.com>
2021-10-18 11:04:26 +0200

    updated code

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+let postArray = []
+
+function renderPost() {
+    let html = ''
+    for (let post of posts){
+        html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr>
+        `
+    }
+    document.querySelector('#blog-post').innerHTML = html 
+}
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(Response => Response.json())
     .then(data => {
         const posts = data.slice(0, 5) 
-        let html = ''
-        for (let post of posts){
-            html += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr>
-            `
-        }
-        document.querySelector('#blog-post').innerHTML = html 
+        renderPost()
     })
 
 document.getElementById("btn").addEventListener('submit', function(e){
@@ -21,6 +27,18 @@ document.getElementById("btn").addEventListener('submit', function(e){
         title : postTitle,
         body : postBody
     }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(post => {
+        postArray.unshift(post)
+        renderPost()
+        document.getElementById("new-post").reset();
+    })
 })
 
 
